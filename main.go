232913ae46b87983eeb79572b646278331b78cea
dboyer <dboyer@solutionsmedias360.com>
2015-08-18 21:01:55 -0400

    experimenting with wav library

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strings"
+
+	"github.com/cryptix/wav"
+)
+
+const (
+	bits = 32
+	rate = 44100
 )
 
 //directly use byte for optimization
@@ -62,13 +70,18 @@ func main() {
 
 	scanner := bufio.NewScanner(inputFile)
 
+	var morseCode string
+
 	for scanner.Scan() {
-		fmt.Println(translateToMorse(scanner.Text()))
+		morseCode += translateToMorse(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(scanner.Err())
 	}
+
+	fmt.Println(morseCode)
+	writeWavFile(morseCode)
 }
 
 func translateToMorse(line string) string {
@@ -82,4 +95,36 @@ func translateToMorse(line string) string {
 	}
 
 	return morseCode
+}
+
+func writeWavFile(morseCode string) {
+
+	wavOut, err := os.Create("output.wav")
+	checkErr(err)
+	defer wavOut.Close()
+
+	meta := wav.File{
+		Channels:        1,
+		SampleRate:      rate,
+		SignificantBits: bits,
+	}
+
+	writer, err := meta.NewWriter(wavOut)
+	checkErr(err)
+	defer writer.Close()
+
+	var freq float64
+	freq = 0.1
+
+	for n := 0; n < 50*rate; n += 1 {
+		y := int32(0.8 * math.Pow(2, bits-1) * math.Sin(freq*float64(n)))
+		err = writer.WriteInt32(y)
+		checkErr(err)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
